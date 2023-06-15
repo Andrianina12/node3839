99d04cc70e4d1f48419bab1c82ebc78b09baca9b
Andrianina12 <ramorasata.andrianina@gmail.com>
2023-06-15 20:14:10 +0300

    Init project

---
+++ model/user.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+
+let UsersSchema = Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+UsersSchema.plugin(aggregatePaginate);
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+// le nom de la collection (par défaut Users) sera au pluriel, 
+// soit Users
+// Si on met un nom "proche", Mongoose choisira la collection
+// dont le nom est le plus proche
+module.exports = mongoose.model('Users', UsersSchema);
---
+++ routes/users.js
@@ -0,0 +1,86 @@
+let User = require('../model/user');
+
+// Récupérer tous les users (GET)
+function getUsersSansPagination(req, res){
+    User.find((err, users) => {
+        if(err){
+            res.send(err)
+        }
+
+        res.send(users);
+    });
+}
+
+function getUsers(req, res) {
+    var aggregateQuery = User.aggregate();
+    
+    User.aggregatePaginate(aggregateQuery,
+      {
+        page: parseInt(req.query.page) || 1,
+        limit: parseInt(req.query.limit) || 10,
+      },
+      (err, users) => {
+        if (err) {
+          res.send(err);
+        }
+        res.send(users);
+      }
+    );
+   }
+   
+// Récupérer un user par son id (GET)
+function getUser(req, res){
+    let userId = req.params.id;
+
+    User.findOne({id: userId}, (err, user) =>{
+        if(err){res.send(err)}
+        res.json(user);
+    })
+}
+
+// Ajout d'un user (POST)
+function postUser(req, res){
+    let user = new User();
+    user.email = req.body.email;
+    user.nom = req.body.nom;
+    user.password = req.body.password;
+
+    console.log("POST user reçu :");
+    console.log(user)
+
+    user.save( (err) => {
+        if(err){
+            res.send('cant post user ', err);
+        }
+        res.json({ message: `${user.nom} saved!`})
+    })
+}
+
+// Update d'un user (PUT)
+function updateUser(req, res) {
+    console.log("UPDATE recu user : ");
+    console.log(req.body);
+    
+    User.findByIdAndUpdate(req.body._id, req.body, {new: true}, (err, user) => {
+        if (err) {
+            console.log(err);
+            res.send(err)
+        } else {
+          res.json({message: user.nom + 'updated'})
+        }
+
+      // console.log('updated ', user)
+    });
+
+}
+
+// suppression d'un user (DELETE)
+function deleteUser(req, res) {
+
+    User.findByIdAndRemove(req.params.id, (err, user) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({message: `${user.nom} deleted`});
+    })
+}
