fc7549ed131df520a393bbc85924025922050f57
Andrianina12 <ramorasata.andrianina@gmail.com>
2023-06-19 00:08:09 +0300

    Update auth.js (error)

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,7 @@ function login(req, res) {
         if (!user) return res.status(404).send('No user found.');
         
         var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+        if (!passwordIsValid) return res.status(400).send('Mot de passe incorrect');
         
         var token = jwt.sign({ id: user._id }, config.secret, {
           expiresIn: 86400 // expires in 24 hours
